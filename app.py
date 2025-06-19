60a3df4233cd5cf5b23fdae02ff6d63b16cad536
Josafat <josafat@example.com>
2025-06-19 14:01:20 -0600

    modificacion2

--- app.py
+++ app.py
@@ -4,25 +4,22 @@ import joblib
 app = Flask(__name__)
 model = joblib.load('modelo_entrenado.pkl')
 
+# Diccionario para convertir números a nombres
+target_names = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
+
 @app.route('/')
 def home():
     return "¡Hola, Mundo!"
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.get_json()
-
-    # Validación básica
-    if not data or 'features' not in data:
-        return jsonify({'error': 'Debes enviar un JSON con la clave "features"'}), 400
-
-    features = data['features']
-
-    if len(features) != 4:
-        return jsonify({'error': 'El vector de características debe tener 4 valores'}), 400
-
-    prediction = model.predict([features])
-    return jsonify({'prediction': int(prediction[0])})
+    data = request.json
+    prediction = model.predict([data['features']])
+    predicted_class = target_names[prediction[0]]
+    return jsonify({
+        'prediction': int(prediction[0]),
+        'class_name': predicted_class
+    })
 
 if __name__ == '__main__':
     app.run(debug=True)
